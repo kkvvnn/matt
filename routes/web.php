dd4f1855e7e1d4f0cc64784c879b4a500379c006
kkvvnn <kkvvnn89@gmail.com>
2020-03-30 19:21:50 +0500

    add routes /users/...

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,11 @@ Route::get('/', function () {
 Route::get('hello', function () {
     return 'Hello, World';
 });
+
+Route::get('users/{age?}', function ($age = 0) {
+    return '<h1>возраст: ' . $age . '</h1>';
+})->where(['age' => '[0-9]+']);
+
+Route::get('users/{username?}', function ($username = '') {
+    return '<h1>Имя: ' . $username . '</h1>';
+})->where(['username' => '[A-Za-z]+']);
